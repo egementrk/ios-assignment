e5aa689b0e99fbe8fbde5f18c2272f5a1cc59296
Egemen TÜRK <egementurk@192.168.1.37>
2024-09-08 23:52:58 +0300

    case-add Closures

---
+++ ArabamAssignment/Constants/Closures.swift
@@ -0,0 +1,11 @@
+//
+//  Closures.swift
+//  ArabamAssignment
+//
+//  Created by Egemen TÜRK on 2.09.2024.
+//
+
+public typealias VoidClosure = (() -> Void)
+public typealias IntClosure = ((Int) -> Void)
+public typealias BoolClosure = ((Bool) -> Void)
+public typealias StringClosure = ((String) -> Void)

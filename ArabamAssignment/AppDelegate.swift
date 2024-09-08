b267127d457d47a2d502f95db8da00a0162d5e0c
Egemen TÜRK <egementurk@192.168.1.37>
2024-09-08 23:59:12 +0300

    case-move AppDelegate and Info.plist

--- ArabamAssignment/AppDelegate.swift
+++ ArabamAssignment/AppDelegate.swift
@@ -14,6 +14,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            self.window = window
+            AppRouter.shared.startApp(window: window, dataProvider: apiDataProvider)
+        }
         return true
     }
 
